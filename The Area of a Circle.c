38778a9a57356084f9f95b764fda613d8a785da2
SharathChandra-BG <81412360+SharathChandra-BG@users.noreply.github.com>
2021-03-26 18:34:15 +0530

    Update The Area of a Circle.c

--- The Area of a Circle.c
+++ The Area of a Circle.c
@@ -1,13 +1,13 @@
 //To find the area of a circles
 #include<stdio.h>
 int main(){
-    int r;
+    int radius; 
     float pi = 3.14; //Making the value of pi as 3.14
     
 
-    printf("Enter th Value of the Radius of the circle: ",r);
+    printf("Enter th Value of the Radius of the circle: ",radius);
     scanf("%d",&r);
-    float area = r*r*pi;
+    float area = radius*radius*pi;
 
     printf("The area of the circle = %f",area);
 
